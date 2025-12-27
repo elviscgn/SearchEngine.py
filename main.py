531e43633c8385095666f4b9f10f78a911ffe163
Zinhle Hlongwane <zinhlehlongwani56@gmail.com>
2025-12-27 15:49:45 +0200

    Define a function called add

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 class SearchEngine:
     def __init__(self):
-        
         self.index = {}
+
+    def add(self, document_id, text):
+        text = text.lower()
+
+        words = text.split()
+
+        for word in words:
+            if word not in self.index:
+                self.index[word] = set()
+
+            self.index[word].add(document_id)
